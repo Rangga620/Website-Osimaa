0fa1b4368c68de3be14302c611036b9d236d0589
Rangga620 <Jannuartaranggaashraffy@gmail.com>
2024-08-19 15:13:55 +0700

    Update Struktur A.js

--- Struktur A.js
+++ Struktur A.js
@@ -9,7 +9,7 @@ function showDetails(type) {
     if (type === 'putra') {
         window.location.href = 'Struktur B Putra.html'; // Ganti dengan nama file yang sesuai
     } else if (type === 'putri') {
-        window.location.href = 'Struktur B Putri.html'; // Ganti dengan nama file yang sesuai
+        window.location.href = 'struktur B Putri.html'; // Ganti dengan nama file yang sesuai
     }
 }
 function goBack() {
